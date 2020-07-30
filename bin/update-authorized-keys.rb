0050b104f3a840b53f07c1099331cc11ab739b51
poornima-moj <poornima.krishnasamy@digital.justice.gov.uk>
2020-07-30 13:15:33 +0100

    Updated for repo name, added comments and fixed the 'error parsing JSON' by using strict_encode

--- bin/update-authorized-keys.rb
+++ bin/update-authorized-keys.rb
@@ -13,49 +13,53 @@ require 'securerandom'
 
 ORG = "ministryofjustice"
 TEAM_NAME = "webops"
-REPO_NAME = "https://github.com/poornima-krishnasamy/cloud-platform-terraform-bastion"
+REPO_NAME = "https://github.com/ministryofjustice/cloud-platform-terraform-bastion"
 
 GITHUB_GRAPHQL_API_URL = "https://api.github.com/graphql"
 
-GITHUB_REST_API_URL = "https://api.github.com/repos/poornima-krishnasamy/cloud-platform-terraform-bastion" 
+GITHUB_REST_API_URL = "https://api.github.com/repos/ministryofjustice/cloud-platform-terraform-bastion" 
 
 BRANCH = SecureRandom.alphanumeric
 
-# Takes a module name, returns the value of the last release defined in the
-# corresponding github repo.
+# Build the autorized_keys file for the given organisation and team,
+# create a branch with random alphanumeric on the cloud-platform-terraform-bastion repo, 
+# and create a commit to replace the existing file authorized_keys.txt with the newly generated one
+# Lastly create a Pull Request against the newly created branch and the commit.
 
 def main
 
   encoded_authorized_keys = build_authorized_keys  
   create_new_branch_commit(encoded_authorized_keys)
-  create_pull_request()
+  puts create_pull_request()
 
 end
 
 def build_authorized_keys
 
-  json = run_query(
+  json = post_query(
       json: {query: get_publickeys_query()}.to_json,
       token: ENV.fetch("GITHUB_TOKEN"),
       api_url: GITHUB_GRAPHQL_API_URL
     )
-
   members = JSON.parse(json)
     .dig("data", "organization", "team", "members","nodes")
 
   authorized_keys = '' 
   members.each do |member|
-    authorized_keys += "# "+format(member["name"])+"\n"
+    authorized_keys +=  "# "+format(member["name"]) + "\n"
     member['publicKeys']["edges"].each do |node|
-      authorized_keys += node["node"]["key"] + "\n"
+      authorized_keys += node["node"]["key"] + " @" + format(member["login"]) + "\n"
     end
   end
-  encoded_authorized_keys = Base64.encode64(authorized_keys).strip
+ 
+  encoded_authorized_keys = Base64.strict_encode64(authorized_keys)
+
 end
 
 
 def create_new_branch_commit(encoded_authorized_keys)
 
+  # Get the main branch sha
   json = get_query(
       token: ENV.fetch("GITHUB_TOKEN"),
       api_url: GITHUB_REST_API_URL + "/git/ref/heads/main" 
@@ -63,14 +67,14 @@ def create_new_branch_commit(encoded_authorized_keys)
 
   sha = JSON.parse(json).dig("object", "sha")
 
-  json = run_query(
+  # Create a reference which is basically creating a branch
+  json = post_query(
       json: get_create_branch_query(sha),
       token: ENV.fetch("GITHUB_TOKEN"),
       api_url: GITHUB_REST_API_URL + "/git/refs" 
     )
   
-  # add_files
-  
+  # Get the latest sha for the content
   api_url = GITHUB_REST_API_URL + "/contents/files/authorized_keys.txt"
 
   json = get_query(
@@ -79,9 +83,12 @@ def create_new_branch_commit(encoded_authorized_keys)
     )
 
     content_sha = JSON.parse(json).dig("sha")
-
+    
+    # Send the PUT request with the encoded authorized_key content 
+    content = get_update_content_query(content_sha,encoded_authorized_keys)
+    
     json = put_query(
-    json: get_update_content_query(content_sha,encoded_authorized_keys),
+    json: content,
     token: ENV.fetch("GITHUB_TOKEN"),
     api_url: api_url
   )
@@ -91,8 +98,8 @@ end
 
 def create_pull_request
 
-  json = run_query(
-      json: {query: get_pull_request_query()}.to_json,
+  json = post_query(
+      json: get_pull_request_query(),
       token: ENV.fetch("GITHUB_TOKEN"),
       api_url: GITHUB_REST_API_URL + "/pulls"
     )
@@ -113,7 +120,7 @@ def get_query(params)
     response.body
 end
 
-def run_query(params)
+def post_query(params)
   token = params.fetch(:token)
   json = params.fetch(:json)
   url = params.fetch(:api_url)
@@ -122,7 +129,6 @@ def run_query(params)
 
   uri = URI.parse(url)
   resp = Net::HTTP.post(uri, json, headers)
-  code = resp.code
   resp.body
 end
 
@@ -131,14 +137,14 @@ def put_query(params)
   token = params.fetch(:token)
   json = params.fetch(:json)
   url = params.fetch(:api_url)
-
+  
   headers = {"Authorization" => "bearer #{token}"}
 
   uri = URI.parse(url)
   http = Net::HTTP.new(uri.host, uri.port)
   http.use_ssl = true
   req = Net::HTTP::Put.new(uri)
-  req["Accept"] = "application/json"
+  req["Accept"] = "application/vnd.github.v3+json",
   req["Authorization"] = "bearer #{token}" 
   req.body = json
   response = http.request(req)
@@ -200,8 +206,9 @@ def get_publickeys_query()
             members {
                 nodes {
                 id
+                login
                 name
-                publicKeys(first: 1) {
+                publicKeys(first: 10) {
                     edges {
                     node {
                         key
